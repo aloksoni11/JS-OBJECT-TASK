f4e4caf039ce9b516fe2eaef0fd7608a879d87c5
aloksoni11 <sonialok5777@gmail.com>
2019-05-16 20:24:38 +0530

    First Init

---
+++ js/main.js
@@ -0,0 +1,174 @@
+var employee = {
+    "contacts": [
+        {
+            "id": "c200"
+            , "name": "Ravi Tamada"
+            , "email": "ravi@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c201"
+            , "name": "Johnny Depp"
+            , "email": "johnny_depp@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c202"
+            , "name": "Leonardo Dicaprio"
+            , "email": "leonardo_dicaprio@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c203"
+            , "name": "John Wayne"
+            , "email": "john_wayne@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c204"
+            , "name": "Angelina Jolie"
+            , "email": "angelina_jolie@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "female"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c205"
+            , "name": "Dido"
+            , "email": "dido@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "female"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c206"
+            , "name": "Adele"
+            , "email": "adele@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "female"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c207"
+            , "name": "Hugh Jackman"
+            , "email": "hugh_jackman@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c208"
+            , "name": "Will Smith"
+            , "email": "will_smith@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c209"
+            , "name": "Clint Eastwood"
+            , "email": "clint_eastwood@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c2010"
+            , "name": "Barack Obama"
+            , "email": "barack_obama@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c2011"
+            , "name": "Kate Winslet"
+            , "email": "kate_winslet@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "female"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+                , {
+            "id": "c2012"
+            , "name": "Eminem"
+            , "email": "eminem@gmail.com"
+            , "address": "xx-xx-xxxx,x - street, x - country"
+            , "gender": "male"
+            , "phone": {
+                "mobile": "+91 0000000000"
+                , "home": "00 000000"
+                , "office": "00 000000"
+            }
+        }
+    ]
+};
+
+function showData() {
+    var x = document.getElementById("add-data");
+    var y = document.getElementById("title");
+    y.classList.remove("dnone");
+    for (var i = 0; i <= employee.contacts.length; i++) {
+        var name = employee.contacts[i].name;
+        var Email = employee.contacts[i].email;
+        var add = employee.contacts[i].address;
+        var gender = employee.contacts[i].gender;
+        console.log(i);
+        x.insertAdjacentHTML("beforeEnd", "<div class='col-md-3'>" + name + "</div><div class='col-md-3'>" + Email + "</div><div class='col-md-3'>" + add + "</div><div class='col-md-3'>" + gender + "</div>");
+    }
+}
